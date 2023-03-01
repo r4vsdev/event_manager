e784ca78b6da57e035e63701d6729791a5b80653
r4vsdev <r4vsdev@gmail.com>
2023-03-01 13:59:46 -0300

    Add clean_phone method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -4,6 +4,19 @@ def clean_zicode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
+def clean_phone(phone)
+  phone = phone.to_s.gsub(/\D/, '')
+  if phone.size < 10 || phone.size > 11
+    'Valid number not given'
+  elsif phone.size == 11 && phone[0] != 1
+    'Valid number not given'
+  elsif phone.size == 11 && phone[0] == 1
+    phone[1..-1]
+  else
+    phone
+  end
+end
+
 puts 'EventManager initialized.'
 
 contents = CSV.open(
@@ -15,8 +28,7 @@ contents = CSV.open(
 contents.each do |col|
   name = col[:first_name]
   zipcode = clean_zicode(col[:zipcode])
+  phone = clean_phone(col[:homephone])
 
-
-
-  puts "#{name} #{zipcode}"
+  puts "#{name} #{zipcode} #{phone}"
 end
