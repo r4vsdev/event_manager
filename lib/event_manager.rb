a30dff5ab6ac2eaeb687c64e1827cb75010c6fc1
r4vsdev <r4vsdev@gmail.com>
2023-03-02 01:43:43 -0300

    Add get_hour method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 require 'csv'
+require 'date'
+# require 'time'
 
 def clean_zicode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
@@ -11,11 +15,19 @@ def clean_phone(phone)
   elsif phone.size == 11 && phone[0] != 1
     'Valid number not given'
   elsif phone.size == 11 && phone[0] == 1
-    phone[1..-1]
+    phone[1..]
   else
     phone
   end
 end
+
+def get_hour(reg_date)
+  DateTime.strptime(reg_date, '%m/%d/%y %H:%M').hour
+end
+
+# def clean_date(reg_date)
+#   Date.strptime(reg_date.split(' ').first, '%m/%d/%y').wday
+# end
 
 puts 'EventManager initialized.'
 
@@ -25,10 +37,16 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
+hours = []
 contents.each do |col|
-  name = col[:first_name]
-  zipcode = clean_zicode(col[:zipcode])
-  phone = clean_phone(col[:homephone])
+  # name = col[:first_name]
+  # zipcode = clean_zicode(col[:zipcode])
+  # phone = clean_phone(col[:homephone])
+  hours << get_hour(col[:regdate])
 
-  puts "#{name} #{zipcode} #{phone}"
+  # puts "#{name} #{zipcode} #{phone}"
 end
+peak = []
+hours.tally.each { |k, v| peak << k if v == hours.tally.values.max }
+
+puts "peak hours = #{peak}"
